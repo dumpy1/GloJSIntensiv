cb054459b784c57b1e811b00c7fd3be823751d4f
evgeny <e.chertov@gmail.com>
2019-10-17 08:58:34 +0300

    now accurately fulfilled the main task

---
+++ script.js
@@ -0,0 +1,25 @@
+var money = 1;
+
+var income = 2 ;
+
+var addExpenses = true;
+
+let deposit = 4;
+
+const mission = 'win';
+
+const period = 'month';
+
+
+alert ('сообщение с любым текстом');
+
+console.log('сообщение с любым текстом');
+
+console.log('money: ', money);
+console.log('income: ', income);
+console.log('addExpenses: ', addExpenses);
+console.log('deposit: ', deposit);
+console.log('mission: ', mission);
+console.log('period: ', period);
+
+console.log('похоже, что всё работает!!!');
