7d88aa044c04143ad61e74c528b953e4b2941ecc
evgeny <e.chertov@gmail.com>
2019-10-22 13:49:38 +0300

    complete lesson3

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 'use strict';
 
-var money = 19800;
+var money = prompt('Ваш месячный доход?');
+console.log('money: ', typeof(money));
 
 var income = "1500" ;
 
-var addExpenses = "Комуналка, Кредит, алименты";
+var addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+addExpenses = addExpenses.split(',');
+console.log('addExpenses: ', addExpenses);
 
-let deposit = true;
+console.log('addExpenses: ', typeof(addExpenses));
+
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log('deposit: ', typeof(deposit));
+
+let mandatoryExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let mandatoryExpenses1Cost = prompt('Во сколько это обойдется?');
+mandatoryExpenses1Cost *= 1;
+
+let mandatoryExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let mandatoryExpenses2Cost = prompt('Во сколько это обойдется?');
+mandatoryExpenses2Cost *= 1;
+console.log('mandatoryExpenses2Cost: ', typeof(mandatoryExpenses2Cost));
+
+let budgetMonth  = money - (mandatoryExpenses1Cost + mandatoryExpenses2Cost);
+
+console.log('budgetMonth: ', budgetMonth);
 
 const mission = 458016.52;
 
-const period = 6;
+let period = mission / budgetMonth;
+console.log('period: ', Math.ceil(period));
+
+let budgetDay = budgetMonth / 30;
+
+console.log('budgetDay: ', Math.floor(budgetDay));
+
+console.log('остаток:', money % 30);
 
 
-
-console.log('typeof money: ' +typeof(money));
-console.log('typeof income: ' +typeof(income));
-console.log('typeof deposit: ' +typeof(deposit));
-console.log('income.length: ' +income.length);
-console.log('Период ' + (period) + ' месяцев ');
-console.log('Цель заработать ' + (mission) + ' рублей' );
-
-addExpenses = addExpenses.toLocaleLowerCase ();
-let arr = addExpenses.split(',');
-console.log('arr: ', arr);
-
-let budgetDay = money/30;
-
-console.log('budgetDay: ', budgetDay);
-
-console.log(money % 30);
+   if (budgetDay >= 800) {
+    alert ('Высокий уровень дохода');
+} else if (budgetDay < 800 && budgetDay >= 300) {
+    alert ('Средний уровень дохода');
+} else if (budgetDay < 300 && budgetDay >= 0) {
+    alert ('Низкий уровень дохода');
+} else {
+    alert ('Что то пошло не так');
+}
 
 
 
